6b066e2650a83c870960139bd9f6501397ca5376
aloksharma10 <aloksharma10969@gmail.com>
2025-05-03 15:33:58 +0530

    setup tsconfig.json for all apps and packaage

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,6 +1,9 @@
+import { prisma } from "@lorrigo/db"
 import { Button } from "@lorrigo/ui/components/button"
 
-export default function Page() {
+export default async function Page() {
+  const users = await prisma.user.findMany()
+  console.log(users)
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
